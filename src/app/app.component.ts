14243e07e71c9215fca662385156eba3e084b32e
Jitendra Patwa <jitendrapatwa135@gmail.com>
2018-09-22 23:31:25 +0530

    kline exchange chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Kline from 'kline';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  	console.log('Kline', Kline);
+
+  	    var kline = new Kline({
+        element: "#kline_container",
+        width: 1200,
+        height: 650,
+        theme: 'dark', // light/dark
+        language: 'en-us', // zh-cn/en-us/zh-tw
+        ranges: ["1w", "1d", "1h", "30m", "15m", "5m", "1m", "line"],
+        symbol: "BTC",
+        symbolName: "BTC/USD",
+        type: "poll", // poll/socket
+        url: "/assets/mock.json",
+        limit: 1000,
+        intervalTime: 5000,
+        debug: true,
+        showTrade: true,
+        onResize: function(width, height) {
+            console.log("chart resized: " + width + " " + height);
+        }
+    });
+
+    kline.draw();
+    kline.toggleTrade();
+
+  }
 }
